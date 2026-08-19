fc0cb3f11d0aacf874cf5282aeafac22c151c380
mehulwithai <mehulytwithai@gmail.com>
2026-08-19 23:43:50 +0530

    feat(analytics): add Whop tracking pixel script to root layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next"
+import Script from 'next/script'
 import './globals.css'
 import { ThemeProvider } from '../components/ThemeProvider'
 import Navbar from '../components/Navbar'
@@ -67,6 +68,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body>
+        <Script
+          id="whop-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");whop.setScope("biz_u3LbT7ERPKmwwH");whop.track("page");`
+          }}
+        />
         <ThemeProvider>
           <Navbar />
           <main style={{ minHeight: 'calc(100vh - 56px)' }}>
